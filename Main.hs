156fc50451d195f8b83598d36d15b08da4b62fd3
Josiah Berkebile <praenato14@gmail.com>
2014-06-23 00:37:35 -0500

    retrieve current branch and print to terminal

--- Main.hs
+++ Main.hs
@@ -1,8 +1,26 @@
 import System.Directory
+import System.Process
 import Data.Text as T
-import Git
+import Control.Monad
+import System.Exit
+
+type ProcessResponse = IO (ExitCode, String, String)
+type BranchName = IO (Maybe String)
 
 main :: IO ()
 main = do
-    getCurrentDirectory >>= print
+    curDir      <- getCurrentDirectory
+    maybeBranch <- getCurrentBranch
+    case maybeBranch of Just branch -> print $ curDir ++ " " ++ branch
+                        Nothing     -> print curDir
+
+getCurrentBranch :: BranchName
+getCurrentBranch = parseProcessResponse $ readProcessWithExitCode "git" ["rev-parse","--abbrev-ref","HEAD"] []
+
+parseProcessResponse :: ProcessResponse -> IO (Maybe String)
+parseProcessResponse processResponse = do
+    (exitCode,stdOut,stdErr) <- processResponse
+    case exitCode of ExitSuccess      -> return $ Just $ trim stdOut
+                     ExitFailure _    -> return Nothing
+                     where trim = unpack . strip . pack
 
