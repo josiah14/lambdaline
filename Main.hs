df8fb41aa8bf7bd3b90038b23707c03bfef5d323
Josiah Berkebile <praenato14@gmail.com>
2014-06-29 02:27:00 -0500

    work on adding git status detection support

--- Main.hs
+++ Main.hs
@@ -3,9 +3,14 @@ import System.Process
 import Data.Text as T
 import Control.Monad
 import System.Exit
+import Date.List
+import Data.List.Split
+import Control.Applicative
 
 type ProcessResponse = IO (ExitCode, String, String)
 type BranchName = IO (Maybe String)
+
+data RepoStatus = NoChanges | ChangesToAdd | ChangesToCommit
 
 main :: IO ()
 main = do
@@ -24,8 +29,33 @@ parseProcessResponse processResponse = do
                      ExitFailure 128  -> return Nothing
                      ExitFailure _    -> do
                        print exitCode
-                       print $ stdOut ++ stdErr
+                       print $ stdOut ++ " " ++ stdErr
                        return Nothing
                      where trim = unpack . strip . pack
 
+getCurrentRepoStatus :: IO (Maybe RepoStatus)
+getCurrentRepoStatus = parseProcessResponse $ readProcessWithExitCode
 
+getChangesToAdd :: IO (Maybe [String])
+getChangesToAdd = liftM (fmap split "\n")  $ parseProcessResponse $ readProcessWithExitCode "git" ["add","--all","--dry-run"] []
+
+getChangesToCommit :: IO (Maybe [String])
+getChangesToCommit = liftM (fmap split "\n") $ parseProcessResponse $ readProcessWithExitCode "git" ["status","--porcelain"]
+
+stdOutListAny :: IO (Maybe [String]) -> IO (Maybe Boolean)
+stdOutListAny = liftM fmap not . null
+
+hasChangesToAdd :: IO (Maybe Boolean)
+hasChangesToAdd = stdOutListAny getChangesToAdd
+
+hasChangesToCommit :: IO (Maybe Boolean)
+hasChangesToCommit = ((&&) . <$> =<< (stdOutListAny getChangesToCommit)) =<< (not . <$> =<< hasChangesToAdd )
+hasChangesToCommit = do
+    changesToCommit <- getChangesToCommit
+    maybeAdd        <- hasChangesToAdd
+    return do
+      commitsList <- changesToCommit
+      addBool     <- maybeAdd
+      return commitsList && not addBool
+
+hasAnyChanges :: IO (Maybe Boolean)
