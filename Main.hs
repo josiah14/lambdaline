ee7ef60c3962f1aac58ebb1e87c691cb2609a7c6
josiah14 <praenato14@gmail.com>
2014-07-20 00:04:49 -0500

    fix type errors in main, can now easily concat results from IO proc functions into result IO string from Main

--- Main.hs
+++ Main.hs
@@ -6,63 +6,62 @@ import System.Exit
 import Data.List as L
 import Data.List.Split as SP
 import Control.Applicative
+import Data.Maybe
 
 type ProcessResponse = IO (ExitCode, String, String)
 type BranchName = IO (Maybe String)
 
 data RepoStatus = NoChanges | ChangesToAdd | ChangesToCommit | ChangesToAddAndCommit deriving Show
 
--- main :: IO ()
--- main = do
---     curDir      <- getCurrentDirectory
---     maybeBranch <- getCurrentBranch
---     case maybeBranch of Just branch -> print $ curDir ++ " " ++ branch
---                         Nothing     -> print curDir
-
 main :: IO ()
-main = do
-    curDir <- getCurrentDirectory
-    (L.foldr1 (addSegment) [] [getCurrentBranch,getCurrentRepoStatus]) >>= print
-    where addSegment seg prompt = liftM (fmap ((++) prompt)) (liftM (fmap $ L.concat . show) seg)
-
+main =
+  liftM fromJust (L.foldr1
+                    (addSegment)
+                    [
+                      getCurrentBranch,
+                      liftM (fmap show) $ getCurrentRepoStatus,
+                      liftM Just getCurrentDirectory
+                    ]
+                  ) >>= print
+  where addSegment seg prompt = (liftM2 $ liftA2 (++)) seg prompt
 
 getCurrentBranch :: BranchName
 getCurrentBranch = parseProcessResponse $ readProcessWithExitCode "git" ["rev-parse","--abbrev-ref","HEAD"] []
 
 parseProcessResponse :: ProcessResponse -> IO (Maybe String)
 parseProcessResponse processResponse = do
-    (exitCode,stdOut,stdErr) <- processResponse
-    case exitCode of ExitSuccess      -> return $ Just $ trim stdOut
-                     ExitFailure 128  -> return Nothing
-                     ExitFailure _    -> do
-                       print exitCode
-                       print $ stdOut ++ " " ++ stdErr
-                       return Nothing
-                     where trim = unpack . strip . pack
+  (exitCode,stdOut,stdErr) <- processResponse
+  case exitCode of ExitSuccess      -> return $ Just $ trim stdOut
+                   ExitFailure 128  -> return Nothing
+                   ExitFailure _    -> do
+                     print exitCode
+                     print $ stdOut ++ " " ++ stdErr
+                     return Nothing
+  where trim = unpack . strip . pack
 
 getCurrentRepoStatus :: IO (Maybe RepoStatus)
 getCurrentRepoStatus = do
-    maybeChanges      <- hasChanges
-    maybeCommitAndAdd <- hasChangesToAddAndCommit
-    maybeAddOnly      <- hasChangesToCommitButNotAdd
-    maybeCommitOnly   <- hasChangesToAddButNotCommit
-    case (maybeChanges, maybeCommitAndAdd, maybeAddOnly, maybeCommitOnly) of
-      (Just False,_,_,_) -> return $ Just NoChanges
-      (_,_,Just True,_)  -> return $ Just ChangesToAdd
-      (_,_,_,Just True)  -> return $ Just ChangesToCommit
-      (_,Just True,_,_)  -> return $ Just ChangesToAddAndCommit
-      _                   -> return Nothing
+  maybeChanges      <- hasChanges
+  maybeCommitAndAdd <- hasChangesToAddAndCommit
+  maybeAddOnly      <- hasChangesToCommitButNotAdd
+  maybeCommitOnly   <- hasChangesToAddButNotCommit
+  case (maybeChanges, maybeCommitAndAdd, maybeAddOnly, maybeCommitOnly) of
+    (Just False,_,_,_) -> return $ Just NoChanges
+    (_,_,Just True,_)  -> return $ Just ChangesToAdd
+    (_,_,_,Just True)  -> return $ Just ChangesToCommit
+    (_,Just True,_,_)  -> return $ Just ChangesToAddAndCommit
+    _                  -> return Nothing
 
 splitOnNewline :: String -> [String]
 splitOnNewline str = [ s | s <- SP.splitOn "\n" str, not . L.null $ s ]
 
 getChangesToAdd :: IO (Maybe [String])
 getChangesToAdd = liftM (fmap splitOnNewline) $ parseProcessResponse $ gitAddDryRun
-                where gitAddDryRun = readProcessWithExitCode "git" ["add","--all","--dry-run"] []
+  where gitAddDryRun = readProcessWithExitCode "git" ["add","--all","--dry-run"] []
 
 getAllChanges :: IO (Maybe [String])
 getAllChanges = liftM (fmap splitOnNewline) $ parseProcessResponse $ gitStatus
-                   where gitStatus = readProcessWithExitCode "git" ["status","--porcelain"] []
+  where gitStatus = readProcessWithExitCode "git" ["status","--porcelain"] []
 
 stdOutListAny :: IO (Maybe [String]) -> IO (Maybe Bool)
 stdOutListAny = liftM (fmap $ not . L.null)
@@ -75,24 +74,23 @@ hasChanges = stdOutListAny getAllChanges
 
 hasChangesToCommitButNotAdd :: IO (Maybe Bool)
 hasChangesToCommitButNotAdd = do
-    maybeChanges <- hasChanges
-    maybeAdd     <- hasChangesToAdd
-    return $ (&&) <$> maybeChanges <*> (not <$> maybeAdd)
+  maybeChanges <- hasChanges
+  maybeAdd     <- hasChangesToAdd
+  return $ (&&) <$> maybeChanges <*> (not <$> maybeAdd)
 
 -- length is not enough to determine if add changes are equal to all changes
 hasChangesToAddButNotCommit :: IO (Maybe Bool)
 hasChangesToAddButNotCommit = do
-    maybeHasChanges <- hasChanges
-    maybeAdd        <- getChangesToAdd
-    maybeChanges    <- getAllChanges
-    return $ (&&) <$> maybeHasChanges <*> ((==) <$> (L.length <$> maybeChanges) <*> (L.length <$> maybeAdd))
+  maybeHasChanges <- hasChanges
+  maybeAdd        <- getChangesToAdd
+  maybeChanges    <- getAllChanges
+  return $ (&&) <$> maybeHasChanges <*> ((==) <$> (L.length <$> maybeChanges) <*> (L.length <$> maybeAdd))
 
 hasChangesToAddAndCommit :: IO (Maybe Bool)
 hasChangesToAddAndCommit = do
-    maybeHasChanges <- hasChanges
-    maybeCommitNotAdd <- hasChangesToCommitButNotAdd
-    maybeAddNotCommit <- hasChangesToAddButNotCommit
-    return $ (&&) <$> maybeHasChanges <*> ((&&) <$> (not <$> maybeCommitNotAdd) <*> (not <$> maybeAddNotCommit))
-
+  maybeHasChanges   <- hasChanges
+  maybeCommitNotAdd <- hasChangesToCommitButNotAdd
+  maybeAddNotCommit <- hasChangesToAddButNotCommit
+  return $ (&&) <$> maybeHasChanges <*> ((&&) <$> (not <$> maybeCommitNotAdd) <*> (not <$> maybeAddNotCommit))
 
 
