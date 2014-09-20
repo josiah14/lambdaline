a79a003848e03d139a6c94dd5e25fa8ef9ccb109
josiah14 <praenato14@gmail.com>
2014-09-20 10:44:42 -0500

    clean up lint from last change

--- LambdaLine/Segment.hs
+++ LambdaLine/Segment.hs
@@ -8,7 +8,6 @@ module LambdaLine.Segment
 , style
 , symbol
 ) where
-import Control.Applicative
 import Control.Monad
 import Data.Maybe
 import Data.List as L
@@ -18,7 +17,6 @@ type PromptSegment = IO (Maybe String)
 -- Combines 2 segments into a single segment
 (>+<) :: PromptSegment -> PromptSegment -> PromptSegment
 (>+<) = liftM2 $ \segment0 segment1 -> Just $ foldl1 (++) $ catMaybes [segment0, segment1]
--- (>+<) = liftM2 $ \segment0 segment1 -> (++) <$> segment0 <*> segment1
 
 buildMainPrompt :: [PromptSegment] -> String -> IO ()
 buildMainPrompt segments promptSymbol =
